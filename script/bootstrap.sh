d8e6e50937d06d6ba83d19e8ad9efd331c986cae
karthick S <karthick.s@dummy.com>
2022-11-27 02:13:17 +0530

    Four node lab setup - Working Copy

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -6,7 +6,7 @@ python3 -m pip install ansible
 # Generate keypair and distribute it to controller and managed node so ansuser can run playbooks using the keys
 ssh-keygen -b 2048 -t rsa -f /home/ansuser/.ssh/id_rsa -q -N ""
 
-for host in controller managed1; do
+for host in anscontroller ansubuntu ansalpine ansrocky; do
         echo "++ Copying Key to ${host}"
         sshpass -p 'password123' ssh-copy-id -o "StrictHostKeyChecking=no" ansuser@$host
 done
@@ -14,7 +14,6 @@ done
 # Generate sample setup
 mkdir sample_project
 cd sample_project
-clear
 
 # Ansible.cfg file
 echo -e "++ Generating ansible.cfg file\n"
@@ -26,9 +25,10 @@ nocows = 1" > ansible.cfg
 # Hosts file
 echo -e "++ Generating hosts file file\n"
 echo -e "[servers]
-controller
-managed1" > hosts
+anscontroller 
+ansubuntu 
+ansalpine
+ansrocky" > hosts
 
-# Adhoc command
-echo -e "++ Running adhoc command\n"
-ansible servers -m shell -a "python3 --version"
+# Generating sample playbook
+ansible servers -i /home/ansuser/sample_project/hosts -m shell -a "cat /etc/os-release | grep -i PRETTY_NAME" --key-file /home/ansuser/.ssh/id_rsa -o
